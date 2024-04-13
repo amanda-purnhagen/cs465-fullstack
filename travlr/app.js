1614ca9c2da899ab2d724070ebe8dd90992b5380
a-purnhagen <amanda.purnhagen@snhu.edu>
2024-04-13 13:37:35 -0400

    Module 6 completed baseline.

--- travlr/app.js
+++ travlr/app.js
@@ -37,6 +37,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Enable cross-origin resource sharing (CORS)
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
+// Routes to controllers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
