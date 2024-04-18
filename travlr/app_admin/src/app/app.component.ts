ae4e949bf6f843c02f288b29595a6de44821bf6a
a-purnhagen <amanda.purnhagen@snhu.edu>
2024-04-18 17:03:54 -0400

    Module 7 savepoint #2. Still troubleshooting Navbar and Home components.

--- travlr/app_admin/src/app/app.component.ts
+++ travlr/app_admin/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TripListingComponent } from './trip-listing/trip-listing.component';
+//import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  //imports: [CommonModule, RouterOutlet, TripListingComponent, NavbarComponent],
   imports: [CommonModule, RouterOutlet, TripListingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
